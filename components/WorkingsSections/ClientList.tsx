c378ceade3ac72b15fea17cd8fd636a4a52ac6ad
david temitope <temi236topemicheal@gmail.com>
2023-09-15 17:10:34 +0100

    fix

--- components/WorkingsSections/ClientList.tsx
+++ components/WorkingsSections/ClientList.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Trustbank } from "../icons/logo1";
+import { Trustbank, Sec } from "../icons/logo1";
 
-const clients = [Trustbank];
+const clients = [
+  { id: 1, logo: <Trustbank /> },
+  { id: 1, logo: <Sec /> },
+];
 
 type Props = {};
 
@@ -9,7 +12,7 @@ const ClientList = (props: Props) => {
   return (
     <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-4 md:mt-20">
       {clients.map((logo, i) => (
-        <Clientbox key={i} logo={logo} />
+        <Clientbox key={logo.id} logo={logo.logo} />
       ))}
     </div>
   );
@@ -25,8 +28,8 @@ type Propsclient = {
 function Clientbox(props: Propsclient) {
   const { word, logo } = props;
   return (
-    <div className="border-2 border-black rounded-xl py-16 flex flex-col  items-center justify-center px-14">
-      <div className="text-base tracking-tighter h-40 w-40">{logo}</div>
+    <div className="border-2 border-black rounded-xl  flex  items-center justify-center py-10 ">
+      <div className="h-30 w-40">{logo}</div>
     </div>
   );
 }
