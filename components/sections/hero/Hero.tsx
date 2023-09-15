b0084da1aabe87088c96277782211a2d2a974ac2
david temitope <temi236topemicheal@gmail.com>
2023-09-15 14:51:49 +0100

    fixes

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -4,8 +4,8 @@ import { HeroMotionDiv, LineMotionDiv } from "../../ui/MotionDiv";
 
 const Hero = () => {
   return (
-    <HeroMotionDiv className="px-12 md:mt-3 w-full">
-      <h1 className="justify-between flex items-center text-wine-400 uppercase w-full text-[2rem] md:text-[2.5rem] xl:text-[2rem] xxl:text-[4.5rem] tracking-wide font-[800]">
+    <HeroMotionDiv className="md:px-12 md:mt-3 w-full">
+      <h1 className="justify-between flex items-center text-wine-400 uppercase w-full text-[1rem] md:text-[2.5rem] xl:text-[2rem] xxl:text-[4.5rem] tracking-wide font-[800]">
         we&apos;re born to create a simple,clean , beautiful yet purposeful
         africa, one design at a time
       </h1>
