7097f06a8cbb7ab9e0ff58cfadcc2f19edd09f83
david temitope <temi236topemicheal@gmail.com>
2023-09-19 16:04:26 +0100

    Work on the home list page

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -17,13 +17,13 @@ const page = (props: Props) => {
         </div>
         <div className="h-[3px] w-full bg-black mt-10"></div>
 
-        <div className="w-full flex justify-end py-10 flex-col">
+        <div className="w-full flex justify-end py-10 ">
           <div className=" w-full lg:w-[70%] flex gap-y-14 flex-col ">
             <div className="w-full">
               <p className="mb-4 font-bold text-base">
                 Brief TC on what you need
               </p>
-              <div className="h-[190px] lg:w-[400px] py-10 px-4 lg:px-8 border-2 border-black flex flex-col rounded-lg justify-between">
+              <div className="h-[190px] lg:w-[400px] py-10 px-4 lg:px-8 border-2 border-black flex flex-col  justify-between">
                 <p className="text-2xl md:text-3xl font-bold">Project work:</p>
                 <div className="flex justify-evenly lg:justify-between w-full">
                   <SmallBtn> 10,000</SmallBtn>
@@ -33,14 +33,17 @@ const page = (props: Props) => {
               </div>
             </div>
             <div className="w-full">
-              <form className=" lg:w-[600px] py-10 px-4 lg:px-8 border-2 border-black flex flex-col justify-between rounded-lg">
+              <form className=" lg:w-[600px] py-10 px-4 lg:px-8 border-2 border-black flex flex-col justify-between">
                 <p className="text-2xl md:text-3xl font-bold capitalize pb-6 ">
                   more info:
                 </p>
                 <div className="flex justify-between w-full flex-col gap-y-10 mt-10 ">
-                  <Input placeholder="your email" className="border-gray-400" />
+                  <Input
+                    placeholder="your email"
+                    className="border-gray-400 rounded-none"
+                  />
                   <Textarea
-                    className="resize-none border-gray-400"
+                    className="resize-none border-gray-400 rounded-none"
                     placeholder="A few words about your project"
                   />
                 </div>
