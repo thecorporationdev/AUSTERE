6158f84ed09208a2205a917a903ab76457d6b777
david temitope <temi236topemicheal@gmail.com>
2023-09-26 11:58:46 +0100

    work on capacity page

--- components/CapabilitySections/CapabilityHero.tsx
+++ components/CapabilitySections/CapabilityHero.tsx
@@ -8,7 +8,7 @@ type Props = {};
 
 const CapabilityHero = (props: Props) => {
   return (
-    <section className="w-full flex-col  flex md:mt-10 text-black py-32 md:py-24">
+    <section className="w-full flex-col  flex md:mt-10 text-black pt-32 md:py-24">
       <Reveal>
         <div className=" w-full max-md:flex-col text-black mx-auto flex items-left justify-center">
           <h1 className="w-full text-4xl md:text-6xl mx-auto xl:text-8xl font-[500] text-left uppercase md:py-10">
