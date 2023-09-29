b2c9c4c5059c8d9333e959c7d89d3be442ce6912
david temitope <temi236topemicheal@gmail.com>
2023-09-29 18:30:01 +0100

    work on grid

--- app/Workings/page.tsx
+++ app/Workings/page.tsx
@@ -9,7 +9,7 @@ type Props = {};
 const page = (props: Props) => {
   return (
     <>
-      <section className=" max-sm:max-w-[94%] max-md:w-[85%] md:max-w-[90%] mx-auto text-black py-32 md:py-24">
+      <section className=" max-sm:max-w-[94%] sm:w-[85%]  md:w-[90%] md:max-w-[90%] mx-auto text-black py-32 md:py-24">
         <Reveal>
           <div className=" w-full max-md:flex-col mx-auto flex items-center justify-center">
             <h1 className="w-full text-4xl md:text-6xl mx-auto xl:text-8xl font-[500] text-left uppercase md:py-10">
